778d1e88e2e0499dec1bab6bae4163e54af28459
pengjia <PengJia6>
2019-07-06 20:55:49 +0800

    add demo data

---
+++ demo/tumor_normal/tumor_normal.sh
@@ -0,0 +1,2 @@
+../../msisensor-pro scan -d ../reference/reference.fa -o ../reference/reference.list
+../../msisensor-pro msi -d ../reference/reference.list -t test_tumor_sorted.bam -n test_normal_sorted.bam -o test
---
+++ demo/tumor_only/tumor_only.sh
@@ -0,0 +1,3 @@
+../../msisensor-pro scan -d ../reference/reference.fa -o ../reference/reference.list
+../../msisensor-pro baseline -i baselineBam.configure -d ../reference/reference.list -o baseline
+../../msisensor-pro pro -d ./baseline/reference.list_baseline -t test_tumor_sorted.bam -o test_result/test
