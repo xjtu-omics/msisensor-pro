559a6bb77d8d60fea939d27a5a71b99c1c040bf6
pengjia <PengJia6>
2019-07-10 22:59:13 +0800

    remove demo

--- demo/tumor_normal.sh
+++
@@ -1,3 +0,0 @@
-cd tumor_normal
-../../msisensor-pro scan -d ../reference/reference.fa -o ../reference/reference.list
-../../msisensor-pro msi -d ../reference/reference.list -t test_tumor_sorted.bam -n test_normal_sorted.bam -o test
--- demo/tumor_only.sh
+++
@@ -1,5 +0,0 @@
-cd tumor_only
-../../msisensor-pro scan -d ../reference/reference.fa -o ../reference/reference.list
-../../msisensor-pro baseline -i baselineBam.configure -d ../reference/reference.list -o baseline
-mkdir test_result
-../../msisensor-pro pro -d ./baseline/reference.list_baseline -t test_tumor_sorted.bam -o test_result/test
