80a657682c36b43ea504ee8f001e0ff753acc308
pengjia <PengJia6>
2019-07-10 17:39:12 +0800

    update demo

---
+++ demo/cleardata.sh
@@ -0,0 +1,7 @@
+rm ./reference/reference.list
+rm ./tumor_normal/test
+rm ./tumor_normal/test_dis
+rm ./tumor_normal/test_somatic
+rm ./tumor_normal/test_germline
+rm -r ./tumor_only/test_result/*
+rm -r ./tumor_only/baseline
---
+++ demo/tumor_only.sh
@@ -0,0 +1,5 @@
+cd tumor_only
+../../msisensor-pro scan -d ../reference/reference.fa -o ../reference/reference.list
+../../msisensor-pro baseline -i baselineBam.configure -d ../reference/reference.list -o baseline
+mkdir test_result
+../../msisensor-pro pro -d ./baseline/reference.list_baseline -t test_tumor_sorted.bam -o test_result/test
