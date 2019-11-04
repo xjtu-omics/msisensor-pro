abd51cf8725c4248f6bd374e5c0bf67151712d83
pengjia <PengJia6>
2019-11-04 11:09:05 +0800

    optimize output

--- sample.cpp
+++ sample.cpp
@@ -1,28 +1,4 @@
-/*
- * sample.cpp for MSIsensor 
- * Copyright (c) 2013 Beifang Niu && Kai Ye WUGSC All Rights Reserved.
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- */
+
 
 #include <iostream>
 #include <sstream>
@@ -139,8 +115,8 @@ void Sample::hunterIniTumorDisOutput(const std::string &gavePrefix) {
 	                      << "repeat_times"      <<"\t"
 	                      << "repeat_unit_bases" <<"\t"
 	                      << "right_flank_bases" <<"\t"
-	                      << "pro_U"             <<"\t"
-	 	 	 	 	 	  << "pro_V"             <<"\t"
+	                      << "pro_p"             <<"\t"
+	 	 	 	 	 	  << "pro_q"             <<"\t"
 						  << "CovReads"          <<"\t"
 						  << "threshold"         <<"\n";
 	outputAll << "chromosome"        <<"\t"
@@ -149,8 +125,8 @@ void Sample::hunterIniTumorDisOutput(const std::string &gavePrefix) {
 		                      << "repeat_times"      <<"\t"
 		                      << "repeat_unit_bases" <<"\t"
 		                      << "right_flank_bases" <<"\t"
-		                      << "pro_U"             <<"\t"
-		 	 	 	 	 	  << "pro_V"             <<"\t"
+		                      << "pro_p"             <<"\t"
+		 	 	 	 	 	  << "pro_q"             <<"\t"
 							  << "CovReads"          <<"\t"
 							  << "threshold"         <<"\n";
 }//YeLab
@@ -176,8 +152,8 @@ void Sample::trainIniNormalDisOutput(const std::string &gavePrefix) {//Yelab
 							<< "repeat_unit_bases" <<"\t"
 							<< "right_flank_bases" <<"\t"
 							<< "covReads"          <<"\t"
-							<< "pro_u"             <<"\t"
-							<< "pro_v"             <<"\n";
+							<< "pro_p"             <<"\t"
+							<< "pro_q"             <<"\n";
 
 
 }//YeLab
