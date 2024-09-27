1104402cdb1effb8c9dd63aa7196f561ccb25ec4
pengjia <pengjia1110@gmail.com>
2024-09-27 09:30:52 +0800

    update to v1.3

--- cpp/cmds.cpp
+++ cpp/cmds.cpp
@@ -12,7 +12,7 @@
 //#define VERSION "v1.0.a"
 //#define VERSION "v1.1.a"
 // #define VERSION "v1.2.0"
-#define VERSION "v1.2.2"
+#define VERSION "v1.3.0"
 //https://github.com/xjtu-omics/msisensor-pro/archive/1.1.a.tar.gz
 //https://github.com/xjtu-omics/msisensor-pro/archive/v1.2.0.tar.gz
 // update by Peng Jia  v1.2.0
